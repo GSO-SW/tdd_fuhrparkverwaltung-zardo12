6e63f601d2edb32b7077bd8306787513238c061f
[DZardasht <zardasht.z2@gso.schule.koeln>
2022-12-09 09:05:08 +0100

    lösung für den Fahren_SteigertKilometerstand implementiert

---
+++ Fuhrparkverwaltung/Auto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuhrparkverwaltung
+{
+    public class Auto
+    {
+        private int kilometerstand;
+
+        public Auto(int kilometerstand)
+        {
+            this.kilometerstand = kilometerstand;
+        }
+
+        public int Kilometerstand 
+        {
+            get { return kilometerstand; }
+        }
+
+        public void Fahren(int streckeInKilometern)
+        {
+            kilometerstand += streckeInKilometern;
+        }
+    }
+}
